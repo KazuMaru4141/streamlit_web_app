80682bb8f2f4db0269c276b41869dd5663f8874d
KazMaru4141 <kazumaru4141@gmail.com>
2024-02-03 05:58:09 +0900

    Update refresh timing

--- pages/page1.py
+++ pages/page1.py
@@ -11,58 +11,76 @@ from SpotifyAPI import SpotifyCtrl
 
 import datetime
 
-sp = SpotifyCtrl
-auth_manager, spotify = sp.create_spotify()
-
 st.title('Currently Playing Track')
 col1, col2 = st.columns(2)
 
+if 'trackInfo' not in st.session_state:
+    st.session_state.trackInfo = {}
+    st.session_state.trackInfo["trackName"] = ""
+    st.session_state.trackInfo["artistName"] = ""
+    st.session_state.trackInfo["artistID"] = ""
+    st.session_state.trackInfo["albumName"] = ""
+    st.session_state.trackInfo["albumID"] = ""
+    st.session_state.trackInfo["albumURL"] = ""
+    st.session_state.trackInfo["releaseDate"] = ""
+    st.session_state.trackInfo["albumImg"] = ""
+    st.session_state.trackInfo["genre"] = ""
+    st.session_state.trackInfo["related"] = []
+
+sp = SpotifyCtrl
+auth_manager, spotify = sp.create_spotify()
 currentTrack = spotify.current_user_playing_track()
 
 if currentTrack != None:
-    trackName = currentTrack["item"]["name"]
-    artistName = currentTrack["item"]["artists"][0]["name"]
-    artistID = currentTrack["item"]["artists"][0]["id"]
-    albumName = currentTrack["item"]["album"]["name"]
-    albumID = currentTrack["item"]["album"]["id"]
-    albumURL = currentTrack["item"]["album"]["external_urls"]["spotify"]
-    releaseDate = currentTrack["item"]["album"]["release_date"]
-    albumImg = currentTrack["item"]["album"]["images"][0]["url"]
+    if st.session_state.trackInfo["trackName"] != currentTrack["item"]["name"]:        
+        st.session_state.trackInfo["trackName"] = currentTrack["item"]["name"]
+        st.session_state.trackInfo["artistName"] = currentTrack["item"]["artists"][0]["name"]
+        st.session_state.trackInfo["artistID"] = currentTrack["item"]["artists"][0]["id"]
+        st.session_state.trackInfo["albumName"] = currentTrack["item"]["album"]["name"]
+        st.session_state.trackInfo["albumID"] = currentTrack["item"]["album"]["id"]
+        st.session_state.trackInfo["albumURL"] = currentTrack["item"]["album"]["external_urls"]["spotify"]
+        st.session_state.trackInfo["releaseDate"] = currentTrack["item"]["album"]["release_date"]
+        st.session_state.trackInfo["albumImg"] = currentTrack["item"]["album"]["images"][0]["url"]
 
-    artistInfo = spotify.artist(artistID)
-    relatedArtists = spotify.artist_related_artists(artistID)
+        artistInfo = spotify.artist(st.session_state.trackInfo["artistID"])
+        relatedArtists = spotify.artist_related_artists(st.session_state.trackInfo["artistID"])
+        st.session_state.trackInfo["genre"] = artistInfo["genres"]
 
-    related = []
-    for artist in relatedArtists["artists"]:
-        appendList = [artist["name"], artist["external_urls"]["spotify"]]
-        related.append(appendList)
-    
+        related = []
+        for artist in relatedArtists["artists"]:
+            appendList = [artist["name"], artist["external_urls"]["spotify"]]
+            related.append(appendList)
+        st.session_state.trackInfo["related"] = related
+        
     with col1:
-        st.image(albumImg, width=200)
-    
+        st.image(st.session_state.trackInfo["albumImg"], width=200)
+        
     with col2:
-        st.text(f'■ Album Name : {albumName}')
-        st.text(f'■ Artist Name : {artistName}')
-        st.text(f'■ Track Name : {trackName}')
-        st.text(f'■ Release Date : {releaseDate}')
-    
-    albumURL
-    if artistInfo["genres"] != []:
-        st.text(f'■ Genre : {", ".join(artistInfo["genres"])}')
+        st.write(f'■ Album Name : {st.session_state.trackInfo["albumName"]}')
+        st.write(f'■ Artist Name : {st.session_state.trackInfo["artistName"]}')
+        st.write(f'■ Track Name : {st.session_state.trackInfo["trackName"]}')
+        st.write(f'■ Release Date : {st.session_state.trackInfo["releaseDate"]}')
+        
+    st.session_state.trackInfo["albumURL"]
+    if st.session_state.trackInfo["genre"] != []:
+        st.write(f'■ Genre : {", ".join(st.session_state.trackInfo["genre"])}')
     else:
-        st.text(f'■ Genre : -')
-    st.text(f'■ Related Artists')
+        st.write(f'■ Genre : -')
+    st.write(f'■ Related Artists')
     count = 0
     output = []
 
-    for artist in related:
+    info = []
+    for artist in st.session_state.trackInfo["related"]:
         link = artist[1]
         #print(link)
-        st.text(f'{artist[0]}')
-        st.markdown(link, unsafe_allow_html=True)        
-    
+        st.write(f'{artist[0]}')
+        st.markdown(link, unsafe_allow_html=True) 
+        
 else:
     st.text(f'Track is not playing')
+    
+    
 
 # update every 5sec
 st_autorefresh(interval=15000, key="dataframerefresh")
