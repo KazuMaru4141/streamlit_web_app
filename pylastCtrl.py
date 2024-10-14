3631c79996170e8ba31e68d510c54acd4be4d774
KazMaru4141 <kazumaru4141@gmail.com>
2024-10-15 06:30:48 +0900

    test

--- pylastCtrl.py
+++ pylastCtrl.py
@@ -48,7 +48,7 @@ class pylastCtrl:
         ja = get_localzone()
         current_time = datetime.datetime.now(tz=pytz.timezone("Asia/Tokyo"))
         
-        start_time = datetime.datetime(current_time.year, current_time.month, current_time.day, 0, 0, 0, 0)
+        start_time = datetime.datetime(current_time.year, current_time.month, current_time.day, 0, 0, 0, 0, tzinfo=pytz.timezone("Asia/Tokyo"))
         
         st.write(current_time.utcoffset())
         st.write(f'current time {current_time.year} {current_time.month} {current_time.day} {current_time.hour} {current_time.minute} {current_time.second}')
