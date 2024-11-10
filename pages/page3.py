360325bcd2df86655f4022a9621a9477c21c6aed
kazmaru4141 <kazmaru4141@gmail.com>
2024-11-10 16:37:09 +0900

    create on like/save thread

--- pages/page3.py
+++ pages/page3.py
@@ -1,22 +1,29 @@
 import dataclasses
 import threading
 import time
+import pytz
+import datetime
+import pylast
 
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
-from streamlit_gsheets import GSheetsConnection
+
 from pylastCtrl import pylastCtrl
-import pylast
+from SpreadSheetAPI import GspreadCtrl
+from SpotifyAPI import SpotifyCtrl
 
 class Worker(threading.Thread):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        print("init Worker")
         self.pc = pylastCtrl
         self.lastfm_network = self.pc.getNetwork()
         self.lastfm_user = self.pc.getUser(self.lastfm_network)
         self.playCountToday = self.pc.getPlayCountToday(self.lastfm_user)
         self.overallPlayCount = self.pc.getOverallPlayCount(self.lastfm_user)        
         self.now_playing = self.pc.getNowPlaying(self.lastfm_user)
+        
+        self.i = 0
         
         if self.now_playing != None:
             self.artist_name = self.now_playing.artist.get_name()
@@ -35,7 +42,8 @@ class Worker(threading.Thread):
         while not self.stop_req.wait(0):
             time.sleep(1)
             self.now_playing = self.pc.getNowPlaying(self.lastfm_user)
-            
+            self.i = self.i + 1
+
             if self.now_playing != None:
                 trackName = self.now_playing.get_name()
                 if trackName != self.track_title:
@@ -64,11 +72,31 @@ class Worker(threading.Thread):
         self.albumPlayCount = ""
         self.trackPlayCount = ""
 
-#@st.experimental_singleton
+class SpreadSheetUpdater(threading.Thread):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.onLiked = threading.Event()
+        self.onSaved = threading.Event()
+        
+        print("init SpreadSheetUpdater")
+        gs = GspreadCtrl
+        self.ws, self.wb, self.LikedInfo = gs.connect_gspread(st.secrets.SP_SHEET_KEY.Key_LikedSongs)
+        print("spread sheet read")
+    
+    def run(self):
+        while (1):
+            if self.onLiked.wait(0) == True:
+                print("Likedボタン押されたよ")
+            
+            if self.onSaved.wait(0) == True:
+                print("Savedボタン押されたよ")
+            
+
 @st.cache_resource
 @dataclasses.dataclass
 class ThreadManager:
     worker = None
+    SpreadSheetUpdater = None
     
     def get_worker(self):
         return self.worker
@@ -81,37 +109,104 @@ class ThreadManager:
             self.stop_worker()
         self.worker = Worker(daemon=True)
         self.worker.start()
+        
+        self.worker2 = SpreadSheetUpdater(daemon=True)
+        self.worker2.start()
+        
         return self.worker
     
     def stop_worker(self):
         self.worker.stop_req.set()
         self.worker.join()
         self.worker = None
-
-def main():
-    conn = st.connection("gsheets", type=GSheetsConnection)
-    df = conn.read()
-    for row in df.itertuples():
-        st.write(f"{row.TrackName}")
     
-    thread_manager = ThreadManager()
-
-    with st.sidebar:
-        if st.button('Start worker', disabled=thread_manager.is_running()):
-            worker = thread_manager.start_worker()
-#            st.experimental_rerun()
-            
-        if st.button('Stop worker', disabled=not thread_manager.is_running()):
-            thread_manager.stop_worker()
-#            st.experimental_rerun()
+    def onLiked(self):
+        self.worker2.onLiked.set()
     
+    def onSaved(self):
+        self.worker2.onSaved.set()
+
+#def onclickLiked():
+    # gs = GspreadCtrl
+    # SP_SHEET_KEY = st.secrets.SP_SHEET_KEY.Key_LikedSongs
+    # ws, wb, LikedInfo = gs.connect_gspread(SP_SHEET_KEY)
+    # trackIdList = ws.col_values(6)
+    
+    # if st.session_state.trackInfo["trackID"] not in trackIdList: 
+    #     dt_now = dt_now = datetime.datetime.now(tz=pytz.timezone("Asia/Tokyo"))
+    #     today = str(dt_now.year) + "-" + str(dt_now.month) + "-" + str(dt_now.day)
+    #     appendList = []
+    #     appendList.append([
+    #         today,
+    #         st.session_state.trackInfo["trackName"],
+    #         st.session_state.trackInfo["albumName"],
+    #         st.session_state.trackInfo["artistName"],
+    #         st.session_state.trackInfo["albumImg"],
+    #         st.session_state.trackInfo["trackID"],
+    #         "",
+    #         st.session_state.trackInfo["trackURL"],
+    #         str(1),
+    #     ])
+    #     ws.append_rows(appendList)
+    #     sp.addLikedTrackToPlaylist(spotify, st.session_state.trackInfo["trackURI"])
+    #     st.write(f'Successfully Added')
+    # else:
+    #     cell = ws.find(st.session_state.trackInfo["trackName"])
+    #     row = int(cell.row)
+    #     if (ws.cell(row, 9).value == None):
+    #         ws.update_cell(cell.row, 9, "1")
+    #     else:
+    #         impression = int(ws.cell(row, 9).value)
+    #         impression = impression + 1
+    #         ws.update_cell(cell.row, 9, impression)
+    #     st.write(f'Already Added')
+
+#def onclickSaved():
+    # gs = GspreadCtrl
+    # SP_SHEET_KEY = st.secrets.SP_SHEET_KEY.key_SpotifySavedAlbums
+    # ws, wb, SpreadInfo = gs.connect_gspread(SP_SHEET_KEY)
+    # dt_now = dt_now = datetime.datetime.now(tz=pytz.timezone("Asia/Tokyo"))
+    # today = str(dt_now.year) + "-" + str(dt_now.month) + "-" + str(dt_now.day)
+    # appendList = []
+    # appendList.append([
+    #     today,
+    #     "",
+    #     st.session_state.trackInfo["albumName"],
+    #     st.session_state.trackInfo["artistName"],
+    #     st.session_state.trackInfo["albumImg"],
+    #     st.session_state.trackInfo["artistImg"],
+    #     st.session_state.trackInfo["albumID"],
+    #     st.session_state.trackInfo["albumURL"],
+    #     st.session_state.trackInfo["artistID"],
+    #     st.session_state.trackInfo["artistURL"],
+    #     st.session_state.trackInfo["total_tracks"],
+    #     0,
+    #     0,
+    #     "",
+    #     "",
+    #     st.session_state.trackInfo["artistPopularity"],
+    #     "",
+    #     st.session_state.trackInfo["type"],
+    #     st.session_state.trackInfo["releaseDate"],
+    #     ", ".join(st.session_state.trackInfo["genre"]),
+    #     ""
+    # ])
+    # print(appendList)
+    # ws.append_rows(appendList)
+    # st.write(f'Successfully Saved!')
+    
+def main():    
+    thread_manager = ThreadManager()    
     if not thread_manager.is_running():
-        st.markdown('No worker running.')
+        worker = thread_manager.start_worker()
     else:
         worker = thread_manager.get_worker()
         
         if worker.now_playing != None:
-            st.image(worker.album_cover_image, width=100)  
+            st.markdown(f'{worker.i}')
+            st.image(worker.album_cover_image, width=100)
+            st.button('♥️', on_click=thread_manager.onLiked)
+            st.button('✅', on_click=thread_manager.onSaved)
             st.markdown(f'{worker.track_title} by {worker.artist_name} ({worker.release_date})')
             st.markdown(f'todays play count {worker.playCountToday}')
             st.markdown(f'overall play count {worker.overallPlayCount}')
